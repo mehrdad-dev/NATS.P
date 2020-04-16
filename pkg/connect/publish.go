238e346bca11e143d02023be6a0285ea7971cc9b
mehrdad-dev <mehrdad.dev@yahoo.com>
2020-04-16 15:34:19 +0430

    fix publish

--- pkg/connect/publish.go
+++ pkg/connect/publish.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	nats "github.com/nats-io/nats.go"
 	"natsp/pkg/model"
-	"reflect"
 	"strings"
 )
 
@@ -15,17 +14,17 @@ func Publish (nc *nats.EncodedConn, sbj string, message model.Model) {
 		fmt.Println("ERR IN READ SUBJECT")
 	}
 
-	e := reflect.ValueOf(&message).Elem()
-	for i := 0; i < e.NumField(); i++ {
-		fmt.Sprintf("Enter value of %s (type=%s)", e.Type().Field(i).Name, e.Type().Field(i).Type)
-		t := e.Type().Field(i).Type
-		v := make(e.Type().Field(i).Type,0)
-
-		e.Field(i).Set()
-
-		reflect.ValueOf(&n).Elem().FieldByName("N").SetInt(7)
-
-	}
+	//e := reflect.ValueOf(&message).Elem()
+	//for i := 0; i < e.NumField(); i++ {
+	//	fmt.Sprintf("Enter value of %s (type=%s)", e.Type().Field(i).Name, e.Type().Field(i).Type)
+	//	t := e.Type().Field(i).Type
+	//	v := make(e.Type().Field(i).Type,0)
+	//
+	//	e.Field(i).Set()
+	//
+	//	reflect.ValueOf(&n).Elem().FieldByName("N").SetInt(7)
+	//
+	//}
 
 	err = nc.Publish(strings.TrimSpace(sbj), message)
 	defer nc.Close()
