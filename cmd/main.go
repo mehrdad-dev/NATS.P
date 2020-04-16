297a90c62347fe350a14e701ac029550a9675f5a
mehrdad-dev <mehrdad.dev@yahoo.com>
2020-04-16 15:27:03 +0430

    update cmd/main,go

--- cmd/main.go
+++ cmd/main.go
@@ -2,25 +2,61 @@ package main
 
 import (
 	"fmt"
-	"nats/completer"
+	"github.com/nats-io/nats.go"
+	"log"
+	"natsp/pkg/connect"
+	"natsp/pkg/figlet"
+	"natsp/pkg/file"
+
 	"github.com/c-bata/go-prompt"
 )
 
-func select_function(function string) {
-	switch function {
-	case "Models":
-	case "Publish":
-	case "Subscribe":
-	case "Help":
-	case "About":
-	default:
-		log.Fatal("method not found !")
-		completer.terminal()
-	}
+var nc *nats.EncodedConn
+
+func executor(in string) {
+	fmt.Println("Your input: " + in)
+		switch in {
+		case "Struct":
+			file.GenerateStruct()
+		case "Connect":
+			nc = connect.NewConnection()
+		case "Publish":
+			//connect.Publish()
+		case "Subscribe":
+		case "Help":
+		case "About":
+		default:
+			log.Fatal("method not found !")
+		}
 }
 
+func completer(in prompt.Document) []prompt.Suggest {
+	s := []prompt.Suggest{
+		{Text: "Struct", Description: "Add your struct file"},
+		{Text: "Connect", Description: "Connect to NATS"},
+		{Text: "Publish", Description: "Publish with your struct"},
+		{Text: "Subscribe", Description: "Subscribe with your struct"},
+		{Text: "Help", Description: "Help for work with NATS.P"},
+		{Text: "About", Description: "About NATS.P"},
+	}
+
+	return prompt.FilterHasPrefix(s, in.GetWordBeforeCursor(), true)
+}
+
+
 func main() {
-	fmt.Println("Please select table.")
-	t := prompt.Input("> ", completer)
-	fmt.Println("You selected " + t)
+	print("\033[H\033[2J")
+	figlet.ShowFiglet("NATS.P")
+	fmt.Println("Select Command:")
+
+	p := prompt.New(
+		executor,
+		completer,
+		prompt.OptionPrefix(">>> "),
+		prompt.OptionTitle("NATS.P: A Simple NATS Prompt"),
+		prompt.OptionPrefixTextColor(prompt.Yellow),
+		prompt.OptionPreviewSuggestionTextColor(prompt.Yellow),
+		prompt.OptionSelectedSuggestionBGColor(prompt.Black),
+		prompt.OptionSuggestionBGColor(prompt.Black))
+	p.Run()
 }
